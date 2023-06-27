a1f6fcbb62934ff9910aab592811ffe6e67b68f0
Will Gillette <wgillette02@gmail.com>
2023-06-27 19:29:36 -0400

    Set up

--- index.js
+++
@@ -1,6 +0,0 @@
-const { spawnSync } = require('child_process');
-exports.discordBot = (e, callback) => {
-  // Run the bot
-  const child = spawnSync('node', ['bot.js'], { stdio: 'inherit' });
-  callback(null, 'Success');
-};
